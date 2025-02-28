5c2477a89738088f77030f9e23a370bfd953758c
Ahmad-U19 <ahmedoppal2592@gmail.com>
2025-02-28 03:47:52 -0800

    Initial Changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-import Greet from './Greet';
 
+let name="Ahmad";
 function App() {
   return (
-   <div className="App">
-    <Greet name='Ahmad' heroName='Uppal' />
-    <Greet name='Abdullah' heroName='Chitta' />
-    <Greet name='Zohaib' heroName='Moulvi' />
-   </div>
+  //  <div className="App">
+  //   <Greet name='Ahmad' heroName='Uppal' />
+  //   <Greet name='Abdullah' heroName='Chitta' />
+  //   <Greet name='Zohaib' heroName='Moulvi' />
+  //  </div>
+  <>
+    <nav>
+      <li>Home</li>
+      <li>About Us</li>
+      <li>Contact Us</li>
+    </nav>
+    <h1>Hello {name}</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur ex dolorum harum quod, tenetur alias neque repudiandae deleniti quo quaerat? Natus nesciunt, itaque suscipit consequatur distinctio ea illum earum assumenda!</p>
+  </>
   );
 }
 
